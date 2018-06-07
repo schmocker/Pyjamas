1ce3aad1b62febb99aa95efd753eb60e29c63b50
mangoldf <f.p.mangold@gmail.com>
2018-06-07 17:38:51 +0200

    Power demand update

---
+++ Models/Power_demand/NN_UCTE_woT/V001/NN_prepfunc/__init__.py
@@ -0,0 +1,6 @@
+from .special_days import func_holiday
+from .special_days import func_easterday
+from .special_days import func_eastersunday
+from .special_days import func_xmasday
+
+from .date_prep import prep_date
