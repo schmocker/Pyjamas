3294a2914bfeb7306f2b119088a9c7451e9f3bb5
mangoldf <f.p.mangold@gmail.com>
2018-06-07 16:59:27 +0200

    Power demand ...

--- Models/Power_demand/NN_UCTE_woT/V001/model.py
+++ Models/Power_demand/NN_UCTE_woT/V001/model.py
@@ -10,7 +10,7 @@ import numpy as np
 import json
 from datetime import datetime, timedelta, date
 import math
-
+from .Matlab_dwq import myfunc
 # define the model class and inherit from class "Supermodel"
 class Model(Supermodel):
     # model constructor
@@ -20,55 +20,46 @@ class Model(Supermodel):
 
         # define inputs
         self.inputs['date'] = Input({'name': 'date'})
-        #self.inputs['dir'] = Input({'name': 'wind direction'})
 
         # define outputs
         self.outputs['p_dem'] = Output({'name': 'power demand'})
 
         # define properties
-        self.properties['h_hub'] = Property(10, {'name': 'hub height'})
-
+        self.properties['offset'] = Property(10, {'name': 'demand offset'})
 
         # define persistent variables
-        #self.pers_variable_0 = 5
+        self.model_pars = None
 
     async def func_birth(self):
-        pass
+        listA = [1,2,3,4] #array
+        dictA = {'alter': 5, 'grösse': 1.2}
+        gr = dictA["grösse"]
 
-    async def func_prep(self):
-        # calculate something
-        prep_result = 3 * 5
-        # pass values to peri function
-        return prep_result
+        path = "path/path/filetxt"
+        with open(path, 'r') as f:
+            json_str = f.read()
+            data = json.loads(json_str)
+
+        self.model_pars = data
+
 
     async def func_peri(self, prep_to_peri=None):
-        prep_result = prep_to_peri
         # get inputs
-        in1 = await self.get_input('v')
-        in2 = await self.get_input('dir')
+        dates = await self.get_input('date')
 
-        # calculate something
-        # One can declare custom functions (eg: see end of file)
-        # If you declare them "async" you will have to "await" them (like "extremely_complex_calculation")
-        # Else one could declare "normal" (blocking) functions as well (like "complex_calculation")
-        out1 = prep_result * self.complex_calculation(in1)
-        out2 = await self.extremely_complex_calculation(in1, in2)
+        # claculations
+        demand = myfunc(dates)
 
         # set output
-        self.set_output("p_el", out1)
-        self.set_output("f_rot", out2)
+        self.set_output("p_dem", demand)
 
-        # pass values to post function
-        outputs = {'out1': out1, 'out2': out2}
-        return outputs
 
-    async def func_post(self, peri_to_post=None):
-        outputs = peri_to_post
-        # do something with the values (eg: overwrite persistent variable)
-        self.pers_variable_0 = outputs['out1']
 
-    async def func_death(self):
-        print("I am dying! Bye bye!")
+
+
+
+
+
 
     # define additional methods (normal)
     def complex_calculation(self, speed):
