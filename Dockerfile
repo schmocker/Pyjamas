b9ad8f1551ecc7bcebf1228e89a8d0e50a1595a5
schmocker <33063045+schmocker@users.noreply.github.com>
2018-06-18 00:00:29 +0200

    full docker support for RPi

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 # Use an official Python runtime as a parent image
 FROM python:3.6-slim
+#RPi: FROM resin/raspberry-pi-python:3.6-slim
+
+# Use an official Python runtime as a parent image
+FROM resin/raspberry-pi-python:3.6-slim
 
 # Set the working directory to /app
 WORKDIR /app
@@ -7,7 +11,11 @@ WORKDIR /app
 # Copy the current directory contents into the container at /app
 ADD . /app
 
-# Install any needed packages specified in requirements.txt
+#RPi: RUN sudo apt-get update
+#RPi: RUN sudo apt-get install build-essential libssl-dev libffi-dev python3-dev
+
+
+# Install any required packages specified in requirements.txt
 RUN pip install --trusted-host pypi.python.org -r requirements.txt
 
 # Make port 80 available to the world outside this container
@@ -18,5 +26,6 @@ EXPOSE 5000
 
 # This is only for some systems like rpi
 CMD "#!/bin/bash"
+
 # Run run.py when the container launches
 CMD ["python", "run.py"]
--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,44 +1,14 @@
 from flask import Flask
-from os import environ as env
 import jinja2
-app = Flask(__name__, instance_relative_config=True) # , template_folder="templates"
 
-app.jinja_loader = jinja2.ChoiceLoader([
-    app.jinja_loader,
-    jinja2.FileSystemLoader('Models'),
-])
+# instantiate Flask app
+app = Flask(__name__)
 
-try:
-    app.config.from_pyfile('config.cfg')
-except:
-    print('no valid file instance/config')
-keys = ['FLASK_HOST',
-           'FLASK_PORT',
-           'FLASK_DEBUG',
-           'DB_USER',
-           'DB_PASSWORD',
-           'DB_HOST',
-           'DB_PORT',
-           'DB_DATABASE',
-           'SECRET_KEY',
-           'SECURITY_PASSWORD_SALT',
-           'SECURITY_REGISTERABLE',
-           'SECURITY_PASSWORD_HASH',
-           'SQLALCHEMY_TRACK_MODIFICATIONS']
+# add /Model as additional template folder for flask to load custom property and result views
+app.jinja_loader = jinja2.ChoiceLoader([ app.jinja_loader,jinja2.FileSystemLoader('Models'),])
 
-for key in keys:
-    value = env.get(key)
+# load configs
+app.config.from_pyfile('config.py')
 
-    if value is not None:
-        if key in ['FLASK_DEBUG', 'SECURITY_REGISTERABLE', 'SQLALCHEMY_TRACK_MODIFICATIONS']:
-            db_value = True if value == '1' else False
-        app.config[key] = value
-
-
-db_values = [app.config.get(db_key) for db_key in ['DB_USER','DB_PASSWORD','DB_HOST','DB_PORT','DB_DATABASE']]
-db_uri = ("mysql+pymysql://{}:{}@{}:{}/{}").format(*db_values)
-app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-
-
+# load routes
 from . import routes
-
