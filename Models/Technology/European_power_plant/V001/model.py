3b555c88e380e3272b1362041390662a2b3772dc
linuswalker <linus.walker@fhnw.ch>
2018-06-26 18:18:10 +0200

    database interpolation started

--- Models/Technology/European_power_plant/V001/model.py
+++ Models/Technology/European_power_plant/V001/model.py
@@ -10,7 +10,7 @@ import os
 from Models.Technology.European_power_plant.V001.db import Base, Kraftwerk, Kraftwerkstyp, Brennstofftyp, \
     Kraftwerksleistung, Brennstoffpreis, Verguetung, Entsorgungspreis, Co2Preis, create_dummy_data
 
-# imports
+# general imports
 import datetime
 import random
 import numpy as np
@@ -63,7 +63,7 @@ class Model(Supermodel):
         # get inputs
         t_in = await self.get_input('t')
         # only first time value for interpolation
-        t = t_in[0]
+        kw_time = t_in[0]
 
 
         # TODO query all data
@@ -74,9 +74,20 @@ class Model(Supermodel):
         db_kw_fk_kwt = self.db.query(Kraftwerk.fk_kraftwerkstyp).order_by(Kraftwerk.id).all()
         db_kw_long = self.db.query(Kraftwerk.long).order_by(Kraftwerk.id).all()
         db_kw_lat = self.db.query(Kraftwerk.lat).order_by(Kraftwerk.id).all()
-        db_kw_pinst = self.db.query(Kraftwerk.power_inst).order_by(Kraftwerk.id).all()
-        db_kw_datetime = self.db.query(Kraftwerk.datetime).order_by(Kraftwerk.id).all()
-        db_kw_spezinfo = self.db.query(Kraftwerk.spez_info).order_by(Kraftwerk.id).all()
+
+        # alternative
+        # db_kw_id = [i.id for i in db_kw]
+        # db_kw_bez = [i.bezeichnung for i in db_kw]
+        # db_kw_fk_kwt = [i.fk_kraftwerkstyp for i in db_kw]
+        # db_kw_long = [i.long for i in db_kw]
+        # db_kw_lat = [i.lat for i in db_kw]
+
+        # query Kraftwerksleistung
+        db_kwl = self.db.query(Kraftwerksleistung).order_by(Kraftwerksleistung.id).all()
+        db_kwl_id = self.db.query(Kraftwerksleistung.id).order_by(Kraftwerksleistung.id).all()
+        db_kwl_fk_kw = self.db.query(Kraftwerksleistung.fk_kraftwerk).order_by(Kraftwerksleistung.id).all()
+        db_kwl_pinst = self.db.query(Kraftwerksleistung.power_inst).order_by(Kraftwerksleistung.id).all()
+        db_kwl_datetime = self.db.query(Kraftwerksleistung.datetime).order_by(Kraftwerksleistung.id).all()
 
         # query Brennstoffpreis
         db_bsp = self.db.query(Brennstoffpreis).all()
@@ -88,7 +99,13 @@ class Model(Supermodel):
         db_co2 = self.db.query(Co2Preis).all()
 
         # query Entsorgungspreis
-        db_ents = self.db.query(Entsorgungspreis).all()
+        db_ents = self.db.query(Entsorgungspreis).order_by(Entsorgungspreis.id).all()
+        db_ents_id = self.db.query(Entsorgungspreis.id).order_by(Entsorgungspreis.id).all()
+        db_ents_fk_kwt = self.db.query(Entsorgungspreis.fk_kraftwerkstyp).order_by(Entsorgungspreis.id).all()
+        db_ents_long = self.db.query(Entsorgungspreis.long).order_by(Entsorgungspreis.id).all()
+        db_ents_lat = self.db.query(Entsorgungspreis.lat).order_by(Entsorgungspreis.id).all()
+        db_ents_datetime = self.db.query(Entsorgungspreis.datetime).order_by(Entsorgungspreis.id).all()
+        db_ents_preis = self.db.query(Entsorgungspreis.preis).order_by(Entsorgungspreis.id).all()
 
         # query Kraftwerksleistung
         db_kwt = self.db.query(Kraftwerksleistung).all()
@@ -99,34 +116,44 @@ class Model(Supermodel):
         # query Vergütung
         db_verg = self.db.query(Verguetung).all()
 
-        # alternative
-        # db_kw_id = [i.id for i in db_kw]
-        # db_kw_bez = [i.bezeichnung for i in db_kw]
-        # db_kw_fk_kwt = [i.fk_kraftwerkstyp for i in db_kw]
-        # db_kw_long = [i.long for i in db_kw]
-        # db_kw_lat = [i.lat for i in db_kw]
-        # db_kw_pinst = [i.power_inst for i in db_kw]
-        # db_kw_datetime = [i.datetime for i in db_kw]
-        # db_kw_spezinfo = [i.spez_info for i in db_kw]
 
         # TODO Interpolationen fertig, outputs ordnen
         # KEV Interpolation
           # get KW_id ---> KWT
-        kev = self.interpol_3d(db_kev_t, db_kev_lat, db_kev_long, db_kev_preis, db_kw_lat, db_kw_long, t)
+        kev = self.interpol_3d(db_kev_t, db_kev_lat, db_kev_long, db_kev_preis, db_kw_lat, db_kw_long, kw_time)
 
         # P_inst Interpolation
-        pinst = self.interpol_1d(db_kw_pinst, ..., t)
+        pinst = self.interpol_1d(db_time, db_values, kw_time)
 
         # Brennstoffpreis Interpolation
           # get KW_id--> KWT-->BST
-        bsp = self.interpol_3d(db_bsp_t, db_bsp_lat, db_bsp_long, db_bsp_preis, db_kw_lat, db_kw_long, t)
+        bsp = self.interpol_3d(db_bsp_t, db_bsp_lat, db_bsp_long, db_bsp_preis, db_kw_lat, db_kw_long, kw_time)
 
         # CO2-Preise Interpolation
-        co2 = self.interpol_3d(db_co2_t, db_co2_lat, db_co2_long, db_co2_preis, db_kw_lat, db_kw_long, t)
+        co2 = self.interpol_3d(db_co2_t, db_co2_lat, db_co2_long, db_co2_preis, db_kw_lat, db_kw_long, kw_time)
 
-        # Entsorgungskosten Interpolation
+        # Entsorgungspreis Interpolation
           # get KW_id ---> KWT
-        ents = self.interpol_3d(db_ents_t, db_ents_lat, db_ents_long, db_ents_preis, db_kw_lat, db_kw_long, t)
+        ents = self.interpol_3d(db_ents_t, db_ents_lat, db_ents_long, db_ents_preis, db_kw_lat, db_kw_long, kw_time)
+
+        # Verguetung Interpolation
+          # get KW_id ---> KWT
+
+        verg = []
+        for kw in db_kw:
+            print("Verguetung", kw.id, kw.bezeichnung)
+            db_verg = kw.kraftwerkstyp.verguetungen
+            db_verg_t = [i.datetime for i in db_verg]
+            db_verg_lat = [i.lat for i in db_verg]
+            db_verg_long = [i.long for i in db_verg]
+            db_verg_beitrag = [i.beitrag for i in db_verg]
+
+            db_kw_lat = kw.lat
+            db_kw_long = kw.long
+
+            verg = verg + [self.interpol_3d(db_verg_t, db_verg_lat, db_verg_long, db_verg_beitrag, db_kw_lat, db_kw_long, kw_time)]
+        verg = np.array(verg)
+
 
 
         # TODO Berechnugnen integrieren für
@@ -211,21 +238,35 @@ class Model(Supermodel):
                 kw_long = np.array([2.5, 7.5, 5])
                 kw_time = t0 + np.array([0.5, 2.5, 5]) * datetime.timedelta(days=365)
 
+        else:
+            db_lat = np.asarray(db_lat)
+            db_long = np.asarray(db_long)
+            db_time = np.asarray(db_time)
+            db_values = np.asarray(db_values)
+            kw_lat = np.asarray(kw_lat)
+            kw_long = np.asarray(kw_long)
+            kw_time = np.asarray(kw_time)
+
         # change time values from datetime to float
         db_timestamp = np.asarray([datetime.datetime.timestamp(i) for i in db_time])
         kw_timestamp = np.asarray([datetime.datetime.timestamp(i) for i in kw_time])
         # arrange inputs for griddata
         xi = np.vstack((kw_lat, kw_long, kw_timestamp))
         gridpoints = np.vstack((db_lat, db_long, db_timestamp))
+
         # interpolate
         interp_nearest = griddata(gridpoints.T, db_values.T, xi.T, method='nearest')
-        interp_linear = griddata(gridpoints.T, db_values.T, xi.T, method='linear')
-        # replace linear NAN with nearest
-        kw_values = np.where(np.isnan(interp_linear), interp_nearest, interp_linear)
+        if db_values.size <= 3:
+            kw_values = interp_nearest
+        else:
+            interp_linear = griddata(gridpoints.T, db_values.T, xi.T, method='linear')
+            # replace Nan in linear with nearest (out of range values)
+            kw_values = np.where(np.isnan(interp_linear), interp_nearest, interp_linear)
 
         if plot_data:
-            print('interp_linear')
-            print(interp_linear)
+            if not db_values.size <= 3:
+                print('interp_linear')
+                print(interp_linear)
             print('interp_nearest')
             print(interp_nearest)
             print('kw_values')
@@ -292,6 +333,11 @@ class Model(Supermodel):
             kw_time = t0 + np.random.rand(4) * 30 * datetime.timedelta(days=30)
             # kw_time = t0 + np.random.rand(1) * 30 * datetime.timedelta(days=30)
 
+        else:
+            db_time = np.asarray(db_time)
+            db_values = np.asarray(db_values)
+            kw_time = np.asarray(kw_time)
+
         # change time values from datetime to float
         time = np.asarray([datetime.datetime.timestamp(i) for i in db_time])
         xi = np.asarray([datetime.datetime.timestamp(i) for i in kw_time])
@@ -332,17 +378,13 @@ def start_db():
 
     # an engine is the real DB
     engine = create_engine('sqlite:///'+db_path)
-
     # delete all tables
     Base.metadata.drop_all(engine)
-
     # create all tables
     Base.metadata.create_all(engine)
-
     # a session is used to communicate with the DB
     Base.metadata.bind = engine
     session = sessionmaker(bind=engine)()
-
     # create dummy data
     create_dummy_data(session)
 
@@ -357,8 +399,7 @@ if __name__ == "__main__":
 
     stop = 1
 
-
-if __name__ == "__main__":
+    # define Input
     dt = 900
     t0 = datetime.datetime.now()
     t = t0 + np.arange(96) * datetime.timedelta(seconds=dt)
