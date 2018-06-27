93242c1fbe1ba777ea69ce1b10370c7e35f6ecfa
agittarius <33063045+schmocker@users.noreply.github.com>
2018-06-27 15:41:21 +0200

    adapt models to port change

--- Models/Math/Sin/V1/model.py
+++ Models/Math/Sin/V1/model.py
@@ -8,12 +8,12 @@ class Model(Supermodel):
         sets the sum of 3 inputs as output
     """
 
-    def __init__(self, uuid, name :str):
+    def __init__(self, uuid, name: str):
         super(Model, self).__init__(uuid,name)
 
-        self.inputs['in'] = Input({'name': 'Numbers', 'unit': '[num]', 'dimensions': []})
+        self.inputs['in'] = Input(name='Numbers', unit='any', info='number or number array')
 
-        self.outputs['sin'] = Output({'name': 'Sin', 'unit': '[num]', 'dimensions': []})
+        self.outputs['sin'] = Output(name='Sin', unit='any', info='number or number array')
 
     async def func_peri(self, prep_to_peri=None):
 
@@ -24,3 +24,14 @@ class Model(Supermodel):
         sin = np.sin(nums)
 
         self.set_output("sin", sin.tolist())
+
+if __name__=='__main__':
+    inputs = {
+        'in': 1
+    }
+
+    properties = {}
+
+    outputs = Model.test(inputs, properties)
+
+    print(outputs)
--- Models/Scheduler/Future_Geneartor/V1/model.py
+++ Models/Scheduler/Future_Geneartor/V1/model.py
@@ -14,12 +14,14 @@ class Model(Supermodel):
         super(Model, self).__init__(uuid,name)
         self.elapsed = 0
 
-        self.inputs['time'] = Input({'name': 'Time', 'unit': 'utc time', 'dimensions': []})
+        self.inputs['time'] = Input(name='Time', unit='s', info='utc time in seconds since epoch')
 
-        self.outputs['times'] = Output({'name': 'Futures', 'unit': 'utc time array', 'dimensions': []})
+        self.outputs['times'] = Output(name='Futures', unit='s', info='utc time array in seconds since epoch')
 
-        self.properties["interval"] = Property(1, float, {'name': 'Interval time', 'unit': 's', 'dimensions': []})
-        self.properties["future_steps"] = Property(0, int, {'name': 'Number of intervals', 'unit': '-', 'dimensions': []})
+        self.properties["interval"] = Property(1, float, name='Interval time',
+                                               unit='s',  info='Time between each time stamps')
+        self.properties["future_steps"] = Property(0, int, name='Number of intervals',
+                                                   unit='-',  info='Number of time stamps')
 
     async def func_birth(self):
         pass
