3b998b058c8d3a99570eeb065fd372de668cdad8
mangoldf <f.p.mangold@gmail.com>
2018-07-05 17:48:37 +0200

    Model weather  - set up historic data  - implemented, but not tested

--- Models/Weather/Weather_1/V001/model.py
+++ Models/Weather/Weather_1/V001/model.py
@@ -1,5 +1,11 @@
 from core import Supermodel
 from core.util import Input, Output, Property
+import csv
+from datetime import datetime
+from Models._utils.time import datetime2utc_time, utc_time2datetime
+import pandas as pd
+import numpy as np
+from pytz import timezone
 
 
 # define the model class and inherit from class "Supermodel"
@@ -66,14 +72,14 @@ class Model(Supermodel):
             # selection of weather model based on mode
             if mode == 'live':
                 #w_data = 1
-                w_data_it = [self.weather_API(long, lat, tt) for tt in dates]
+                w_data_it = [self.weather_API(self, long, lat, tt) for tt in dates]
                 T_it = [item[0] for item in w_data_it]
                 u_it = [item[1] for item in w_data_it]
                 P_it = [item[2] for item in w_data_it]
 
             else:
                 #w_data = 2
-                w_data_it = [self.weather_historic(long, lat, tt, self.get_property('ref_year')) for tt in dates]
+                w_data_it = [self.weather_historic(self, long, lat, tt, self.get_property('ref_year')) for tt in dates]
                 T_it = [item[0] for item in w_data_it]
                 u_it = [item[1] for item in w_data_it]
                 P_it = [item[2] for item in w_data_it]
@@ -89,7 +95,6 @@ class Model(Supermodel):
 
         # set output
         self.set_output("weather_data", weather_data)
-
 
     def weather_API(self, long, lat, date):
 
@@ -112,11 +117,22 @@ class Model(Supermodel):
 
         return w_data_it
 
-    def weather_historic(self, long, lat, date, year):
+    def weather_historic(self, long, lat, date, ref_year):
 
-        T_it = 21
-        u_it = 11
-        P_it = 1001
+        # read weather data dependent on long and lat
+        # and select data of reference year
+        data_hist = self.historic_data_read(self, long, lat, ref_year)
+
+        # read or interpolate weather data dependent on date
+        d_weather = self.historic_interpolation(data_hist, date, ref_year)
+        T_it = d_weather.iloc[0]['Temperature']
+        u_it = d_weather.iloc[0]['Wind_speed']
+        P_it = d_weather.iloc[0]['Radiation']
+
+        # # trivial
+        # T_it = 21
+        # u_it = 11
+        # P_it = 1001
 
         # offset
         T_it = T_it*(1+self.get_property("T_offset")/100)
@@ -126,3 +142,93 @@ class Model(Supermodel):
         w_data_it = [T_it, u_it, P_it]
 
         return w_data_it
+
+    def historic_data_read(self, long, lat, ref_year=2000):
+
+        # select file to be read
+        filename = self.historic_file_selection(long, lat)
+
+        # read file
+        with open(filename, 'r') as f:
+            reader = csv.reader(f)
+            next(reader)
+            data = list(reader)
+            #data = [r for r in reader]
+            #print(data)
+
+        # formatting
+        data_t = list(map(list, zip(*data)))
+        date = [datetime.strptime(x, '%Y-%m-%d %H:%M') for x in data_t[0]]
+        date_s = np.asarray(date)
+        date_s = date_s[np.newaxis, :]
+        date_s = date_s.transpose()
+
+        T_vec = np.asarray([float(x) for x in data_t[1]])
+        u_vec = np.asarray([float(x) for x in data_t[2]])
+        P_vec = np.asarray([float(x) for x in data_t[3]])
+
+        T_vec = T_vec[np.newaxis, :]
+        T_vec = T_vec.transpose()
+        u_vec = u_vec[np.newaxis, :]
+        u_vec = u_vec.transpose()
+        P_vec = P_vec[np.newaxis, :]
+        P_vec = P_vec.transpose()
+
+        data_con = np.concatenate((date_s, T_vec, u_vec, P_vec), axis = 1)
+        dataframe = pd.DataFrame(data_con, columns=['Date', 'Temperature', 'Wind_speed', 'Radiation'])
+        dataframe.sort_values(by=['Date'])
+
+        # select year
+        year_vec = [x.year for x in dataframe['Date']]
+        dataframe['Year'] = year_vec
+        data_sel = dataframe.loc[dataframe['Year'] == ref_year]
+        data_sel = data_sel.drop('Year', axis=1)
+
+        return data_sel
+
+    @staticmethod
+    def historic_file_selection(long, lat):
+
+        # Point 1: Europe - onshore
+        filename = '../dummy_historic/Weather_2000.csv'
+
+        return filename
+
+    @staticmethod
+    def historic_interpolation(data_hist, date, ref_year):
+
+        # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # Zeitzonen nach kontrollieren
+        # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        # check if date is within the list
+        # - manipulate year of date to ref_year
+        # ------------
+        date = 1530801794.9515              # 05.07.2018 16:43 local
+        #date = 1530802800.0                 # 05.07.2018 17:00 local
+        date = int(date)
+        # ------------
+        date = utc_time2datetime(date)
+        # date_UTC = date_UTC.replace(tzinfo=timezone('UTC'))
+        # ? date_local = date_UTC.astimezone(timezone('Europe/Brussels'))
+        date = date.replace(year=ref_year)
+        date = date.replace(tzinfo=None)
+
+        # - compare date with dates in data_hist
+        date_in_list = data_hist.loc[data_hist['Date'] == date]
+        if date_in_list.empty == False:
+            weather_data = data_hist.loc[data_hist['Date'] == date]
+            print('IN LIST')
+        else:
+            data_around = data_hist.iloc[(data_hist['Date'] - date).abs().argsort()[:2]]
+            data_inter = data_around
+            data_inter.loc[-1, 'Date'] = date
+            data_inter = data_inter.sort_values('Date').reset_index(drop=True)
+            data_inter['Temperature'] = pd.to_numeric(data_inter['Temperature'], errors='coerce')
+            data_inter['Wind_speed'] = pd.to_numeric(data_inter['Wind_speed'], errors='coerce')
+            data_inter['Radiation'] = pd.to_numeric(data_inter['Radiation'], errors='coerce')
+            data_inter = data_inter.interpolate(method='linear')    #(method='time')
+            weather_data = data_inter.iloc[[1]]
+            print('NOT IN LIST -> INTERPOLATION')
+
+        return weather_data
