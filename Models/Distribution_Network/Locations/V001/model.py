73dc68ef088e0452255b82ee7d40456312041121
mangoldf <f.p.mangold@gmail.com>
2018-07-06 16:07:57 +0200

    Dist, Power, Weather - update properties

--- Models/Distribution_Network/Locations/V001/model.py
+++ Models/Distribution_Network/Locations/V001/model.py
@@ -14,13 +14,15 @@ class Model(Supermodel):
         #self.inputs['mode'] = Input(name='modus', unit='-', info="modus (live or simulation)")
 
         # define outputs
-        self.outputs['distNets'] = Output(name='Distribution Networks', unit='Location: {Long, Lat}', info='???????')
+        self.outputs['distNets'] = Output(name='Distribution Networks', unit='{Location: [], Latidude: [], Longitude []}', info='???????')
 
         # define properties
         # - distribution network
-        self.properties['distNets'] = Property("{}", str, name='distribution network', unit='-',
+        self.properties['distNets'] = Property(default="{}", data_type=str, name='distribution network', unit='-',
                                                info="distribution network with lon and lat",
-                                               example='{"Baden": {"Lat": 45.255631, "Lon": 23.254853}, "Brugg": {...}, ...}')
+                                               example='{"Baden": {"Lat": 47.47256, "Lon": 8.30850}, '
+                                                       '"Brugg": {"Lat": 47.48420, "Lon": 8.20706}, '
+                                                       '"Olten": {"Lat": 47.35212, "Lon": 7.90801}}')
 
         self.distNets = None
 
