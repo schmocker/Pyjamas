68533facc3772cef6e5363d0410d47927452d59f
mangoldf <f.p.mangold@gmail.com>
2018-07-18 15:17:14 +0200

    Power demand model - cleaning and update

--- Models/Power_demand/NN_UCTE_woT/V001/NN_functions/__init__.py
+++
@@ -1,4 +0,0 @@
-from .NN_functions import func_NeuralNetwork
-from .NN_functions import mapminmax_apply
-from .NN_functions import tansig_apply
-from .NN_functions import mapminmax_reverse
--- Models/Power_demand/NN_UCTE_woT/V001/date_functions/__init__.py
+++
@@ -1,2 +0,0 @@
-from .date_UTC_to_local import date_UTC_to_local
-from .date_local_to_UTC import date_local_to_UTC
